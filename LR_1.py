bee94798f2c7ef628d9e3bf806dff0fb0f08d43d
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-02-10 00:10:41 +0300

    степенная

--- LR_1.py
+++ LR_1.py
@@ -1,9 +1,13 @@
+import math
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot(data, MSQ_arr):
+
+def plot(data, MSQ_arr, STEP_arr):
     plt.plot(data[0], data[1], label="эксп.")
     plt.plot(data[0], MSQ_arr, label="МНК")
+    plt.plot(data[0], STEP_arr, label="СТЕП")
     plt.grid()
     plt.legend()
     plt.show()
@@ -47,5 +51,45 @@ def MSQ(arrX, arrY):
     print(MSQ_Array_Y)
     return MSQ_Array_Y
 
-MSQ_arr =  MSQ(data[0],data[1])
-plot(data, MSQ_arr)
+
+def STEP(arrX, arrY):
+    sumX, sumY, sumX2, sumXY, n = 0, 0, 0, 0, len(arrX)
+    for i in range(0, len(arrX)):
+        sumX += math.log(arrX[i])
+        sumY += math.log(arrY[i])
+        sumX2 += math.log(arrX[i]) * math.log(arrX[i])
+        sumXY += math.log(arrX[i]) * math.log(arrY[i])
+    print(sumX, sumY, sumX2, sumXY)
+    arr1 = [sumX2, sumX]
+    arr2 = [sumX, n]
+    arr3 = [sumXY, sumY]
+    print(arr1, arr2, arr3)
+
+    mainDet = np.linalg.det(np.array([arr1, arr2]))
+    print(round(mainDet, 4))
+    det1 = np.linalg.det(np.array([arr3, arr2]))
+    print(round(det1, 4))
+    det2 = np.linalg.det(np.array([arr1, arr3]))
+    print(round(det2, 4))
+    a = round((det1 / mainDet), 2)
+    b = round((det2 / mainDet), 2)
+    print(a)
+    print(b)
+
+    beta = math.e ** b
+    print(beta)
+
+    STEP_Array_Y = []
+    for i in range(0, n):
+        newY = beta * (arrX[i] ** 1.26)
+        STEP_Array_Y.append(newY)
+
+    print(STEP_Array_Y)
+    return STEP_Array_Y
+
+
+
+
+MSQ_arr = MSQ(data[0], data[1])
+STEP_arr =  STEP(data[0], data[1])
+plot(data, MSQ_arr, STEP_arr)
