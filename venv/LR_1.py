6533fd26da378fafde7ac21f9d94c879847abfa3
Sultan <sultangordovg@gmail.com>
2023-02-09 14:51:28 +0300

    cramer for first in MSQ

--- venv/LR_1.py
+++ venv/LR_1.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-# plt.plot([1,2,3,4])
-# plt.show()
+
+
+plt.plot([1,2,3,4])
+plt.show()
 
 def epsSys(arrX, arrY):
     sumX, sumY, sumX2, sumXY = 0, 0, 0, 0
@@ -11,13 +13,27 @@ def epsSys(arrX, arrY):
         sumX2 += arrX[i] * arrX[i]
         sumXY += arrX[i] * arrY[i]
     print(sumX, sumY, sumX2, sumXY)
-    return sumX, sumY, sumX2, sumXY
+    arr1 = [sumX2, sumX]
+    arr2 = [sumX, len(arrX)]
+    arr3 = [sumXY, sumY]
+    print(arr1, arr2, arr3)
+    return arr1, arr2, arr3
+
 
 # sumX2 * a + sumX * b = sumXY
 # sumX * a + N * b = sumY
 
-def detsAndA_B():
-    mainDet =
+def cramer(arrs):
+    mainDet = np.linalg.det(np.array([arrs[0], arrs[1]]))
+    print(round(mainDet, 4))
+    det1 = np.linalg.det(np.array([arrs[2], arrs[1]]))
+    print(round(det1, 4))
+    det2 = np.linalg.det(np.array([arrs[0], arrs[2]]))
+    print(round(det2, 4))
+    a = round((det1 / mainDet), 2)
+    b = round((det2 / mainDet), 2)
+    print(a)
+    print(b)
 
 
-epsSys([1, 2, 3, 4, 5, 6], [1.0, 1.5, 3.0, 4.5, 7.0, 8.5])
+cramer(epsSys([1, 2, 3, 4, 5, 6], [1.0, 1.5, 3.0, 4.5, 7.0, 8.5]))
